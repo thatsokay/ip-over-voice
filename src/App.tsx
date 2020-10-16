debe6010798de2e44875728c6f41c2b13ecc8fe5
Andre Wong <andre@andre-wong.com>
2020-10-17 00:14:41 +1000

    Added rebass

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import {ThemeProvider} from 'emotion-theming'
+import {Heading} from 'rebass'
+const theme = require('@rebass/preset')
 
-const App = () => <div className="App"></div>
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Heading>IP over Voice</Heading>
+  </ThemeProvider>
+)
 
 export default App
