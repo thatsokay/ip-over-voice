30c84fc6d42bf1417f008c60040d70904e377a85
Andre Wong <andre@andre-wong.com>
2020-10-17 01:49:08 +1000

    Convert chunks to ints

--- src/Speaker.tsx
+++ src/Speaker.tsx
@@ -3,15 +3,23 @@ import React from 'react'
 import {FileData} from './types'
 
 const chunkEvery = (str: string, count: number) =>
-  str.match(new RegExp(`.{1,${count}}`, 'g'))?.map((chunk) => chunk)
+  str.match(new RegExp(`.{1,${count}}`, 'g'))!.map((chunk) => chunk)
+
+const strToInts = (str: string) => str.split('').map((c) => c.charCodeAt(0))
 
 interface Props {
   fileData: FileData
 }
 
 const Speaker: React.FC<Props> = ({fileData}) => {
-  const threeByteGroups = chunkEvery(fileData.data, 4)
-  return <>{JSON.stringify(threeByteGroups)}</>
+  const threeByteChunks = chunkEvery(fileData.data, 4)
+  return (
+    <>
+      {JSON.stringify(
+        threeByteChunks.map((chunk) => atob(chunk)).map(strToInts),
+      )}
+    </>
+  )
 }
 
 export default Speaker
