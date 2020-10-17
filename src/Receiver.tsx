5f25f80e672d94520522c09734b3255099115493
Andre Wong <andre@andre-wong.com>
2020-10-18 00:42:26 +1000

    Receive words

--- src/Receiver.tsx
+++ src/Receiver.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Button} from 'rebass'
 
 import {createRecognition} from './speech'
+import {wordValues} from './PGPWordList'
 
 const recognition = createRecognition()
-recognition.onresult = (event) => console.log(event.results[0]![0]!.transcript)
 
 const Receiver: React.FC = () => {
-  const receive = () => recognition.start()
-  return <Button onClick={receive}>Receive</Button>
+  const [receivedBytes, setReceivedBytes] = useState<number[]>([])
+  useEffect(() => {
+    recognition.onresult = (event) => {
+      const result = event.results[0]![0]!.transcript
+      const receivedByte = wordValues[result.toLowerCase()]
+      if (receivedByte === undefined) {
+        return
+      }
+      setReceivedBytes([...receivedBytes, receivedByte])
+    }
+  }, [receivedBytes])
+  return <Button onClick={() => recognition.start()}>Receive</Button>
 }
 
 export default Receiver
